d972419eae0241432774beb0acc8591c7be12f4e
Qodirxon2000aa <namesuz41@gmail.com>
2026-01-09 12:17:56 +0500

    sas

--- src/components/WelcomeAnimation.jsx
+++ src/components/WelcomeAnimation.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import "./WelcomeAnimation.css";
-import video from "../assets/anime.mp4"; // ❗ faqat mp4
+import video from "../assets/anime.mp4";
 
 const WelcomeAnimation = ({ onFinish }) => {
   return (
     <div className="welcome-screen">
-      <video
-        className="welcome-video"
-        src={video}
-        autoPlay
-        muted
-        playsInline
-        onEnded={() => onFinish?.()} // ✅ SAFE
-      />
+      <div className="video-box">
+        <video
+          className="welcome-video"
+          src={video}
+          autoPlay
+          muted
+          playsInline
+          onEnded={() => onFinish?.()}
+        />
+      </div>
     </div>
   );
 };
